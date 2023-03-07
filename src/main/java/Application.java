8d53f2443148ef4cb95ed25fff856d57b981e922
Oleg Kartashev <officinator@gmail.com>
2023-03-08 00:24:19 +0300

    My solution

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -25,27 +25,27 @@ public class Application {
                 System.out.println(gender);
                 System.out.println("age = " + age + " years.");
                 System.out.println(city);
-
-
-                EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
-                System.out.println(employeeDAO.readById(1));
-                for (Employee empl: employeeDAO.readAll() ) {
-                    System.out.println(empl);
-                }
-                employeeDAO.updateGenderById(1,"male");
-                System.out.println(employeeDAO.readById(1));
-
-                // если id города нет в соответствующей таблице то при добавлении пользователя прокидывает исключение, что ожидаемо.
-                Employee newPersonal = new Employee("Freddie", "Mercury", "male", 77,
-                        new City(4, "Kazan"));
-               // /* //замутил чтобы при пробных запусках не плодить лишних записей в базе. Оба метода работают.
-                employeeDAO.create(newPersonal);
-                employeeDAO.deleteById(16);
-                System.out.println("***");
-                for (Employee empl: employeeDAO.readAll() ) {
-                    System.out.println(empl);
-                }
             }
+
+            EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
+            System.out.println(employeeDAO.readById(1));
+            for (Employee empl : employeeDAO.readAll()) {
+                System.out.println(empl);
+            }
+            employeeDAO.updateGenderById(1, "male");
+            System.out.println(employeeDAO.readById(1));
+
+            // если id города нет в соответствующей таблице то при добавлении пользователя прокидывает исключение, что ожидаемо.
+            Employee newPersonal = new Employee("Freddie", "Mercury", "male", 77,
+                    new City(4, "Kazan"));
+            /* замутил чтобы при пробных запусках не плодить лишних записей в базе. Оба метода работают.
+            employeeDAO.create(newPersonal);
+            employeeDAO.deleteById(16);*/
+            System.out.println("***");
+            for (Employee empl : employeeDAO.readAll()) {
+                System.out.println(empl);
+            }
+
 
         } catch (SQLException e) {
             e.printStackTrace();
